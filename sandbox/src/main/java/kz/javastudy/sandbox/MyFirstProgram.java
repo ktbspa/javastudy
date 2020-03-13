7b0a562ae5bd1544f7d4dfcb071a276eea2e1d16
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-13 18:34:25 +0600

    добавлено несколько функций

--- sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
@@ -2,11 +2,23 @@ package kz.javastudy.sandbox;
 
 public class MyFirstProgram {
 public static void main (String[] args) {
-   String somebody="world";
-System.out.println("Hello,"+somebody+"!");
-   double l=6.2;
-   double s=l*l;
-   System.out.println("Площадь квадрата со стороной l ="+ (l*l));
-   System.out.println(s);
+  hello("world");
+   hello("user");
+   hello("Alexei");
+   double l=5;
+   System.out.println("Площадь квадрата со стороной "+l+"="+area(l));
+
+   double a=2;
+   double b=3;
+   System.out.println("Площадь прямоугольника со сторонами "+a+ " и "+ b+" = "+square(a,b));
+ }
+public static void hello(String somebody) {
+   System.out.println("Hello,"+somebody+"!");
+}
+public static double area(double len) {
+   return len*len;
+}
+public static double square(double a, double b) {
+   return a*b;
 }
 }
