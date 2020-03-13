aed4ac21c107e5aab48d8cd6a99ac0deb5ad72a8
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-13 18:12:27 +0600

    промежуточный коммит

--- sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
@@ -2,6 +2,11 @@ package kz.javastudy.sandbox;
 
 public class MyFirstProgram {
 public static void main (String[] args) {
-System.out.println("Hello, world!");
+   String somebody="world";
+System.out.println("Hello,"+somebody+"!");
+   double l=6.2;
+   double s=l*l;
+   System.out.println("Площадь квадрата со стороной l ="+ (l*l));
+   System.out.println(s);
 }
 }
