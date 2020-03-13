ace0a7138db3df16881b89d15caaf93b4ba0a0b8
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-13 19:53:26 +0600

    методы с объектами

--- sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
@@ -5,20 +5,16 @@ public static void main (String[] args) {
   hello("world");
    hello("user");
    hello("Alexei");
-   double l=5;
-   System.out.println("Площадь квадрата со стороной "+l+"="+area(l));
 
-   double a=2;
-   double b=3;
-   System.out.println("Площадь прямоугольника со сторонами "+a+ " и "+ b+" = "+square(a,b));
+   Square s=new Square(9);
+   System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
+
+   Rectangle c=new Rectangle(5.2,6.1);
+   System.out.println("Площадь прямоугольника со сторонами "+c.a+ " и "+ c.b+" = "+c.area());
  }
 public static void hello(String somebody) {
    System.out.println("Hello,"+somebody+"!");
 }
-public static double area(double len) {
-   return len*len;
-}
-public static double square(double a, double b) {
-   return a*b;
-}
+
+
 }
