f22f03c9cf47ff085b68c1095b18d5d78b1af229
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-13 20:44:15 +0600

    test

--- sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kz/javastudy/sandbox/MyFirstProgram.java
@@ -3,18 +3,22 @@ package kz.javastudy.sandbox;
 public class MyFirstProgram {
 public static void main (String[] args) {
   hello("world");
-   hello("user");
-   hello("Alexei");
 
    Square s=new Square(9);
    System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
 
    Rectangle c=new Rectangle(5.2,6.1);
    System.out.println("Площадь прямоугольника со сторонами "+c.a+ " и "+ c.b+" = "+c.area());
+
  }
 public static void hello(String somebody) {
    System.out.println("Hello,"+somebody+"!");
+
 }
 
 
 }
+
+
+
+
