dbc5329db017852d129b95787d8956a3f0f306f5
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-16 17:45:00 +0600

    Изменения по лекции - тест площади квадрата

---
+++ sandbox/src/test/java/kz/javastudy/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package kz.javastudy.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+   @Test
+   public void testArea() {
+      Square s = new Square (4);
+//      assert s.area()==25;
+      Assert.assertEquals(s.area(),25);
+   }
+}
