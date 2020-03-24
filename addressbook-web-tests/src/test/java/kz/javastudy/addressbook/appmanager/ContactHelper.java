bb6eb6f8cb40a7420880d8ff068ed8b0f14b0bd1
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-24 18:15:16 +0600

    задание 5 - часть 1, вспомогательные классы для создания контакта

---
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,64 @@
+package kz.javastudy.addressbook.appmanager;
+
+import kz.javastudy.addressbook.model.ContactData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactHelper {
+    private WebDriver wd;
+
+    public ContactHelper(WebDriver wd) {
+        this.wd=wd;
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("firstname"), contactData.getFirstname());
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("nickname"), contactData.getNickname());
+        type(By.name("company"), contactData.getCompany());
+        type(By.name("address"), contactData.getAddress());
+        type(By.name("home"), contactData.getHomephone());
+        type(By.name("mobile"), contactData.getMobile());
+        type(By.name("work"), contactData.getWork());
+        type(By.name("fax"), contactData.getFax());
+        type(By.name("email"), contactData.getEmail());
+        type(By.name("email2"), contactData.getEmail2());
+        type(By.name("email3"), contactData.getEmail3());
+        type(By.name("homepage"), contactData.getHomepage());
+        click(By.name("bday"));
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
+        click(By.name("bday"));
+        click(By.name("bmonth"));
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
+        click(By.name("bmonth"));
+        type(By.name("byear"), contactData.getByear());
+        click(By.name("aday"));
+        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
+        click(By.name("aday"));
+        click(By.name("amonth"));
+        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAmonth());
+        click(By.name("amonth"));
+        type(By.name("ayear"), contactData.getAyear());
+    }
+
+    private void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public void submitContactCreation() {
+        click(By.xpath("(//input[@name='submit'])[2]"));
+    }
+
+    private void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void gotoAddNewContactPage() {
+        click(By.linkText("add new"));
+    }
+}
--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/ContactCreationTests.java
@@ -7,10 +7,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    gotoAddNewContactPage();
-    fillContactForm(new ContactData("Anthony", "Howard", "Stark", "ironman", "Stark Industries", "NYC", "123", "456", "789", "0", "ironman@avengers.us", "avengers@avengers.us", "shield@shield.us", "www.im.us", "29", "May", "1970", "20", "May", "2008"));
-    submitContactCreation();
-    returnToHomePage();
+    app.getContactHelper().gotoAddNewContactPage();
+    app.getContactHelper().fillContactForm(new ContactData("Anthony", "Howard", "Stark", "ironman", "Stark Industries", "NYC", "123", "456", "789", "0", "ironman@avengers.us", "avengers@avengers.us", "shield@shield.us", "www.im.us", "29", "May", "1970", "20", "May", "2008"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().returnToHomePage();
   }
 
 }
