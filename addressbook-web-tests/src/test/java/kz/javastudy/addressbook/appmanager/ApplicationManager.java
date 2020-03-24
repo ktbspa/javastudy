bedd1d15fec4c32a0d63a529e9491f9e9886e511
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-24 18:16:09 +0600

    промежуточный комит

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package kz.javastudy.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,32 +11,43 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
     WebDriver wd;
 
+    private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
     public void init() {
-      wd = new ChromeDriver();
-      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-      sessionHelper = new SessionHelper(wd);
-      groupHelper = new GroupHelper(wd);
-      navigationHelper = new NavigationHelper(wd);
-      sessionHelper.login("admin", "secret");
-   }
+        sessionHelper = new SessionHelper(wd);
+        groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        sessionHelper.login("admin", "secret");
+    }
 
-   public void stop() {
-      wd.quit();
-   }
+    public void stop() {
+        wd.quit();
+    }
 
-   private boolean isElementPresent(By by) {
-     try {
-       wd.findElement(by);
-       return true;
-     } catch (NoSuchElementException e) {
-       return false;
-     }
-   }
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
@@ -44,4 +56,10 @@ public class ApplicationManager {
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
     }
+
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
+
+
 }
