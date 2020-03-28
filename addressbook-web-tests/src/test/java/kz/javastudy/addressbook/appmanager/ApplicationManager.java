e7166851f48adcb9b73ab36f1749e21638c5fd4b
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-03-28 19:47:04 +0600

    задание 7 - запуск тестов из Chrome и Firefox

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
@@ -29,8 +28,6 @@ public class ApplicationManager {
             wd= new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
