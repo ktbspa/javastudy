9ee3cafc6aee7d679002d7d6c9e169e13692c0bf
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-03 14:19:24 +0600

    4.2 промежуточный коммит

---
+++ sandbox/src/main/java/kz/javastudy/sandbox/Collections.java
@@ -0,0 +1,11 @@
+package kz.javastudy.sandbox;
+
+public class Collections {
+   public static void main (String[] args) {
+      String[] langs = {"java", "c#", "python", "PHP"};
+      for (String l: langs) {
+         System.out.println("I want to study "+l);
+      }
+
+   }
+}
