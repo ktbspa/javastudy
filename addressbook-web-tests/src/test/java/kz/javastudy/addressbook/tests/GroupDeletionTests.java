997660abe3fe4b7d6e163c28676f1ecfb003f6f8
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-04 21:40:30 +0600

    4.6 сравнение коллекций списками

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupDeletionTests.java
@@ -24,7 +24,7 @@ public class GroupDeletionTests extends TestBase {
 
     before.remove(before.size() -1);
     for (int i=0; i<after.size(); i++) {
-      Assert.assertEquals(before.get(i), after.get(i));
+      Assert.assertEquals(before, after);
     }
   }
 
