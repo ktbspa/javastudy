09c9c0f245a32a22bfcb877896c82373678f1e86
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-04 19:49:59 +0600

    4.2 collections

--- sandbox/src/main/java/kz/javastudy/sandbox/Collections.java
+++ sandbox/src/main/java/kz/javastudy/sandbox/Collections.java
@@ -1,9 +1,18 @@
 package kz.javastudy.sandbox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collections {
    public static void main (String[] args) {
       String[] langs = {"java", "c#", "python", "PHP"};
-      for (String l: langs) {
+
+      List<String> languages = new ArrayList<String>();
+      languages.add("Java");
+      languages.add("C#");
+      languages.add("Python");
+
+      for (String l: languages) {
          System.out.println("I want to study "+l);
       }
 
