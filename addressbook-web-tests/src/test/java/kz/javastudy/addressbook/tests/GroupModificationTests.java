3d89f3f73c541af2d53c718a2d34b33d4a00a2ae
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-06 01:34:31 +0600

    4.10 сортировка в GroupModificationTests

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import kz.javastudy.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,6 +30,9 @@ public class GroupModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g2.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
