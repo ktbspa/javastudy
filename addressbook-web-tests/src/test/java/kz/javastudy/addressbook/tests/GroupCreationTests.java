762375aa341606efeb12105ca883c3a9aa324eb5
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-06 01:10:24 +0600

    4.9 лямбда-выражения в GroupCreationTests

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import kz.javastudy.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +14,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("Gamma", "test1", "test2");
+    GroupData group = new GroupData("beta", "test1", "test2");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size()+1);
 
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    int max = 0;
-    for (GroupData g:after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
-}
+    };
+
