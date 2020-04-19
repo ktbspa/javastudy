d035510a14d4b7ebb8a496c6a6da3cb416d9ef05
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-19 23:50:22 +0600

    6.4 параметризация тестовых методов

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/GroupCreationTests.java
@@ -2,24 +2,36 @@ package kz.javastudy.addressbook.tests;
 
 import kz.javastudy.addressbook.model.GroupData;
 import kz.javastudy.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] { new GroupData().withName("Alpha").withHeader("Aheader").withFooter("Afooter")});
+    list.add(new Object[] { new GroupData().withName("Beta").withHeader("Bheader").withFooter("Bfooter")});
+    list.add(new Object[] { new GroupData().withName("Gamma").withHeader("Gheader").withFooter("Gfooter")});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Alpha").withHeader("test1").withFooter("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()+1));
     Groups after = app.group().all();
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test (enabled = false)
   public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
