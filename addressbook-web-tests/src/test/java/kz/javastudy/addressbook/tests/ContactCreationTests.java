66823b669f2562e5a48ebcd28021c0ad853b8718
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-04-20 00:14:16 +0600

    6.5 загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/kz/javastudy/addressbook/tests/ContactCreationTests.java
@@ -2,22 +2,41 @@ package kz.javastudy.addressbook.tests;
 
 import kz.javastudy.addressbook.model.ContactData;
 import kz.javastudy.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line!=null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withMiddlename(split[2])
+      .withNickname(split[3]).withCompany(split[4]).withAddress(split[5]).withHomephone(split[6]).withMobile(split[7])
+      .withWork(split[8]).withFax(split[9]).withEmail(split[10]).withEmail2(split[11]).withEmail3(split[12])
+      .withHomepage(split[13]).withBday(split[14]).withBmonth(split[15]).withByear(split[16]).withAday(split[17])
+      .withAmonth(split[18]).withAyear(split[19]).withGroup(split[20])});
+      line=reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/icon.jpg");
-    ContactData contact = new ContactData().withId(before.size()-1).withFirstname("Anthony").withLastname("Stark").withMiddlename("Howard")
-            .withNickname("ironman").withPhoto(photo).withCompany("Stark Industries").withAddress("StarkTower").withHomephone("123").withMobile("456")
-            .withWork("789").withFax("000").withEmail("ironman@k.kk").withEmail2("email@k.kk").withEmail3("email3@k.kk")
-            .withHomepage("avengers.kk").withBday("29").withBmonth("May").withByear("1970").withAday("20").withAmonth("May").withAyear("2008").withGroup("Alpha");
     app.contact().create(contact, true);
     app.goTo().homePage();
     assertThat(app.contact().count(), equalTo(before.size()+1));
@@ -29,8 +48,9 @@ public class ContactCreationTests extends TestBase {
   public void testBadContactCreation() throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/icon.jpg");
     ContactData contact = new ContactData().withId(before.size()-1).withFirstname("Anthony").withLastname("S'tark").withMiddlename("Howard")
-            .withNickname("ironman").withCompany("Stark Industries").withAddress("StarkTower").withHomephone("123").withMobile("456")
+            .withNickname("ironman").withPhoto(photo).withCompany("Stark Industries").withAddress("StarkTower").withHomephone("123").withMobile("456")
             .withWork("789").withFax("000").withEmail("ironman@k.kk").withEmail2("email@k.kk").withEmail3("email3@k.kk")
             .withHomepage("avengers.kk").withBday("29").withBmonth("May").withByear("1970").withAday("20").withAmonth("May").withAyear("2008").withGroup("Alpha");
     app.contact().create(contact, true);
