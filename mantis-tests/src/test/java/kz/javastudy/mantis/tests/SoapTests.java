a4fb7572ff7d9a4ea0f4d849096352582273e997
ktbspa <62099263+ktbspa@users.noreply.github.com>
2020-05-09 23:50:17 +0600

    дз#19 интеграция тестов с MantisBT (skipIfNotFixed)

--- mantis-tests/src/test/java/kz/javastudy/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/kz/javastudy/mantis/tests/SoapTests.java
@@ -1,5 +1,6 @@
 package kz.javastudy.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
 import kz.javastudy.mantis.model.Issue;
 import kz.javastudy.mantis.model.Project;
 import org.testng.annotations.Test;
@@ -23,9 +24,16 @@ public class SoapTests extends TestBase{
    @Test
    public void testCreateIssue() throws MalformedURLException, ServiceException, RemoteException {
       Set<Project> projects = app.soap().getProjects();
-      Issue issue = new Issue().withSummary("Test issue 2")
-              .withDescription("Test issue description second").withProject(projects.iterator().next());
+      Issue issue = new Issue().withSummary("Test issue 3")
+              .withDescription("Test issue description 3d").withProject(projects.iterator().next());
       Issue created = app.soap().addIssue(issue);
       assertEquals(issue.getSummary(), created.getSummary());
+   }
+
+   @Test
+   public void testSkipIfNotFixed() throws RemoteException, ServiceException, MalformedURLException {
+      int issueId = 0000003;
+      skipIfNotFixed(issueId);
+      System.out.println("the issue " +issueId+ " is successfully resolved. Test is ready to start.");
    }
 }
--- mantis-tests/src/test/java/kz/javastudy/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/kz/javastudy/mantis/tests/TestBase.java
@@ -1,11 +1,20 @@
 package kz.javastudy.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import javassist.tools.rmi.RemoteException;
+import org.hibernate.service.spi.ServiceException;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import kz.javastudy.mantis.appmanager.ApplicationManager;
 
 import java.io.File;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TestBase {
 
@@ -24,4 +33,19 @@ public class TestBase {
        app.stop();
     }
 
+   public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException, javax.xml.rpc.ServiceException, java.rmi.RemoteException {
+      MantisConnectPortType mc = getMantisConnect();
+      IssueData issue = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
+      return !issue.getStatus().getName().equals("resolved");
+   }
+
+   public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException, javax.xml.rpc.ServiceException, java.rmi.RemoteException {
+      if (isIssueOpen(issueId)) {
+         throw new SkipException("Ignored because of issue " + issueId);
+      }
+ }
+   private MantisConnectPortType getMantisConnect() throws javax.xml.rpc.ServiceException, MalformedURLException {
+      return new MantisConnectLocator()
+              .getMantisConnectPort(new URL(app.getProperty("soap.url")));
+   }
 }
